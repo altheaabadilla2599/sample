8ceb06c26ac894e3fb6a7d76c89cefe0d46d744e
altheaabadilla2599 <70379536+altheaabadilla2599@users.noreply.github.com>
2020-09-29 22:51:17 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,200 @@
+<link rel="stylesheet" type="text/css" href="css/content.css">
+
+<div class="logoholder"> 
+
+<div class="design"> </div> 
+<div class="design2"> 
+<div class="toppingshape"></div>
+
+
+</div> 
+
+<div class="introtext"> We make DELICIOUS Cake </div>
+  </div>
+
+
+
+<div id="first-layer">
+
+  <div class="frosting1"></div>
+     <div class="frosting11" ></div>
+	 <div class="frosting111" ></div>
+	 <div class="frosting1111" ></div>
+	 <div class="frosting0" ></div>
+	  <div class="frosting00" ></div>
+	   <div class="frosting000" ></div>
+	
+</div>
+<div id="second-layer">
+  <div class="frosting2" id="frosting2"></div>
+</div>
+  <div id="last-layer">
+    <div class="frosting3" id="frosting3"></div>
+ </div>
+
+
+
+
+<div class="box">
+
+<div class="text2"><center>Creamy Creations </center></div>
+</div>
+ <div class="holder">
+    <div class="shadow"></div>
+    <div class="cake">
+      <div class="first"></div>
+      <div class="frost"></div>
+      <div class="frost2"></div>
+      <div class="frost3"></div>
+    </div>
+  </div>
+
+
+
+<div class="txt"> Joy Full of Sweet Moments
+					<p>Delight in every BITE.</p>
+</div>
+<br>
+<div class="txt1">
+" Love is like a good cake, you never know when it's
+<p> coming, but you'd better eat it when it does!"</p>
+</div>
+<br>
+
+<div class="txt2">
+	- C. JoyBell C. 
+</div>
+
+
+
+<button class="button">Gallery </button>
+<button class="button2">Reviews</button>
+
+
+
+
+   
+ 
+
+<div class="sayings">
+<div class="heart">
+
+</div>
+About us
+</div>
+<div class="sayings2">It all started with a creamy taste of cake that my mother bakes for thanksgiving.
+Creamy Creations was created because of an inspiration.
+<p>We are glady to serve you one of the best cakes that you could imagine.</p>
+<p>Any flavors that you wished and design will be granted with Creamy Creations. Cakes are special every birthday, every celebrations ends</p>
+ <p>with something sweet. It's all about memories</div>	
+
+
+ 
+
+ 
+	<div class="cake-box">
+	<div class="second"></div>
+    <div class="terd"></div>
+    <div class="pers">
+	
+      
+    </div>
+	</div>
+	
+
+	<br>
+	<br>
+	<br>
+	<br>
+	
+
+	<footer>
+	
+	
+	
+	
+	<div class="one">
+		<div class='facebook'></div>
+		<div class="words"> Contact us in Facebook
+						<li>CreamyCreationsPH</li>  </div>
+		</div>
+	
+	<div class="two">
+		<div class='instagram'>
+		  <div class='instagram2'></div>
+		</div>
+		<div class="words2"> Reach us on Instagram
+							<li>@CreamyCreationsPH</li>  </div>
+	</div>
+	<div class="three">
+	
+		<div class = "logo">
+        <div class = "red">
+            <div class = "play"></div>
+        </div>
+		</div>
+		<div class="words3">Tutorials on Youtube
+					<li>CreamyCreationsBakers</li>  </div>
+	</div>	
+
+<div class="cupcakewrapper">
+<div class="cupcake">
+  <div class="frosting">
+    <span class="top"></span>
+    <span class="middle"></span>
+    <span class="bottom"></span>
+   </div>
+   <div class="minicake"></div>
+  <div class="cup">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  
+ 
+ </div>	
+ 
+ <div class="cupcake2">
+  <div class="frostingg">
+    <span class="topp"></span>
+    <span class="middlee"></span>
+    <span class="bottomm"></span>
+   </div>
+   <div class="minicakee"></div>
+  <div class="cupp">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  
+
+ </div>	
+<br><br><br>
+	  <div class="closing">© Copyright 2020 |  All rights Reserved Creamy Creations</div>
+ </div>	
+	
+
+	</footer>
+	
+   
+   
+	
+   
+   
+ 
+ 
+ 
+ 	 
+ 
+ 
+
+
+
+
+	
+	
+
+					 
+ 
